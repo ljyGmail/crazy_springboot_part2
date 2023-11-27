895ef472d14834742c5305d2f91c481cea83c418
liangjinyong <liangjy881@gmail.com>
2023-11-27 15:18:01 +0900

    first commit

---
+++ src/main/java/org/crazyit/app/CrazySpringbootPart2Application.java
@@ -0,0 +1,13 @@
+package org.crazyit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazySpringbootPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazySpringbootPart2Application.class, args);
+	}
+
+}
