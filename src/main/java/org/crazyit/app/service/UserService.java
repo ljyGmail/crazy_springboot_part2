4f1401553240c46762e1ab35b6d2cd99eb47e4d1
liangjinyong <liangjy881@gmail.com>
2023-12-01 17:26:36 +0900

    5.1.3 数据源配置详解 - c3p0

---
+++ src/main/java/org/crazyit/app/service/UserService.java
@@ -0,0 +1,33 @@
+package org.crazyit.app.service;
+
+import org.crazyit.app.dao.UserDao;
+import org.crazyit.app.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+
+@Service
+public class UserService implements CommandLineRunner {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public void run(String... args) throws Exception {
+        testSave();
+    }
+
+    public void testSave() {
+        System.out.println("-----> " + dataSource);
+
+        for (int i = 1; i < 6; i++) {
+            var user = new User("c3p0" + i, '男', 30 + i);
+            userDao.save(user);
+        }
+    }
+}
